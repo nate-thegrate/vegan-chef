ca540f980dbb5febd85dae8a109e159056761068
Nate Wilson <nate.w5687@gmail.com>
2024-01-26 17:31:41 -0700

    convert from Markdown to HTML format

--- compile_recipes/files/__init__.py
+++ compile_recipes/files/__init__.py
@@ -4,4 +4,4 @@ Super nice to have, since imports will work regardless of your working directory
 """
 from . import nutrition_facts
 from . import yaml_dicts
-from .main import yeet_everything, open_recipe
+from .main import generate_index, yeet_everything, export_recipe, copy_into_recipes
